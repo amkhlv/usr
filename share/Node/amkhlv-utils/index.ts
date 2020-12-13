a40309452886836f1b8be0cae067789ea94918b6
Andrei Mikhailov <amkhlv@gmail.com>
2020-12-13 20:36:40 -0300

    request BibTeX from latex2svg server

--- share/Node/amkhlv-utils/index.ts
+++ share/Node/amkhlv-utils/index.ts
@@ -1,5 +1,10 @@
 import proc = require('child_process')
 import xml = require('xml2js')
+import fs = require('fs')
+import os = require('os')
+import path = require('path')
+import querystring = require('querystring');
+const axios = require('axios')
 
 export async function wait(ms: number) {
     return new Promise((resolve, reject) => {
@@ -44,3 +49,57 @@ export function withPassword(
         }
     )
 }
+
+export function getBibTeX(k: string) : Promise<Map<string,string>> {
+  const xmlfile = path.join(os.homedir(), ".config", "amkhlv", "latex2svg.xml")
+  console.log(`About to get BibTeX for: >>>${k}<<<`);
+  return new Promise(
+    (resolve, reject) => {
+      fs.readFile(
+        xmlfile,
+        (err, xmlconf) => {
+          if (err === null) {
+            (new xml.Parser()).parseString(
+              xmlconf,
+              (e:Error, r:any)=> { 
+                const server_params: {server: {host: string, port: string, bibpath: string, token: string}} = r
+                if (e === null) { 
+                  axios({
+                    method: 'POST',
+                    url: `http://${server_params.server.host}:${server_params.server.port}/${server_params.server.bibpath}?${querystring.stringify({'k': k, 'token': server_params.server.token})}`,
+                    headers: {'BystroTeX': 'yes'}
+                  }
+                  ).then((response:any)=> {
+                    (new xml.Parser()).parseString(
+                      response.data, 
+                      (xmlerr:Error, o:{bibentry: {v: Array<{ $ : {key: string}, '_' : string}>}})=> {
+                        if (xmlerr === null) { 
+                          const bibitem = new Map()
+                          for (let x of o['bibentry']['v']) {
+                            bibitem.set(x['$']['key'], x['_'])
+                          }
+                          resolve(bibitem) 
+                        } else { 
+                          reject(xmlerr) 
+                        }
+                      }
+                    )
+                  }).catch((axios_error:Error)=> reject(axios_error))
+                } else { 
+                  reject(e) 
+                }
+              }
+            )
+          } else { 
+            reject(err) 
+          }
+        }
+      )
+    }
+  )
+}
+
+      
+
+
+
