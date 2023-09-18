563b3f3ef45de1faa9c9f7ff082f5aa67db3effd
Andrei Mikhailov <a.mkhlv@gmail.com>
2023-09-17 21:09:41 -0300

    formatting

--- share/Rust/svg-show/src/main.rs
+++ share/Rust/svg-show/src/main.rs
@@ -1,31 +1,32 @@
+use clap::Parser;
 use gtk::prelude::*;
-use gtk::{glib, CssProvider, Application, ApplicationWindow,Box,Button,Entry,Label};
-use gdk4::Display;
-use std::rc::Rc;
+use gtk::{glib, Application, ApplicationWindow, CssProvider};
+use notify::{
+    event::{AccessKind, AccessMode, EventKind},
+    Event, RecursiveMode, Result, Watcher,
+};
 use std::cell::RefCell;
-use std::io::Write;
-use std::path::{Path,PathBuf};
-use std::sync::{Arc,Mutex};
-use std::thread;
-use clap::{Parser,IntoApp};
-use gdk_pixbuf;
-use notify::{Watcher, RecommendedWatcher, RecursiveMode, Result, Event, event::{EventKind,AccessKind,AccessMode}};
-use mime_guess;
+use std::path::{Path, PathBuf};
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
 
 const APP_ID: &str = "com.andreimikhailov.svg-show";
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
-#[clap(author, 
-       version, 
-       about = "
+#[clap(
+    author,
+    version,
+    about = "
         serial tty example
-        ")]
+        "
+)]
 struct Args {
-    /// device
+    /// directory to watch, if any
     #[clap(short, long)]
     directory: Option<String>,
 
+    /// initial file, if any
     #[clap(short, long)]
     file: Option<String>,
 
@@ -33,20 +34,22 @@ struct Args {
     width: Option<i32>,
 
     #[clap(short, long)]
-    height: Option<i32>
-        
+    height: Option<i32>,
 }
 
 struct Situation {
     count: i32,
-    path: Option<PathBuf>
+    path: Option<PathBuf>,
 }
 
 fn main() -> glib::ExitCode {
     // Create a new application
     let app = Application::builder().application_id(APP_ID).build();
 
-    let situation: Arc<Mutex<Situation>> = Arc::new(Mutex::new(Situation {count : 0, path : None }));
+    let situation: Arc<Mutex<Situation>> = Arc::new(Mutex::new(Situation {
+        count: 0,
+        path: None,
+    }));
     let clops = Args::parse();
     // Connect to "activate" signal of `app`
     let maybe_dir = clops.directory.clone();
@@ -58,46 +61,43 @@ fn main() -> glib::ExitCode {
     });
 
     let situation1 = situation.clone();
-    let mut watcher = notify::recommended_watcher(move |res:Result<Event>| {
-        match res {
-            Ok(event) => {
-                match event.kind {
-                    EventKind::Access(AccessKind::Close(AccessMode::Write)) => {
-                        let mut s = situation1.lock().unwrap();
-                        let p = event.paths[0].clone();
-                        let mime = mime_guess::from_path(&p).first_or_octet_stream();
-                        if mime.type_() == mime::IMAGE {
-                            s.path = Some(event.paths[0].clone());
-                            s.count += 1;
-                        } else {
-                            println!("--- MIME type = >>>{}<<<", mime.type_());
-                        }
-
-                    },
-                    _ => {}
+    let mut watcher = notify::recommended_watcher(move |res: Result<Event>| match res {
+        Ok(event) => match event.kind {
+            EventKind::Access(AccessKind::Close(AccessMode::Write)) => {
+                let mut s = situation1.lock().unwrap();
+                let p = event.paths[0].clone();
+                let mime = mime_guess::from_path(p).first_or_octet_stream();
+                if mime.type_() == mime::IMAGE {
+                    s.path = Some(event.paths[0].clone());
+                    s.count += 1;
+                } else {
+                    println!("--- MIME type = >>>{}<<<", mime.type_());
                 }
-            },
-            Err(e) => println!("watch error: {:?}", e),
-        }
-    }).unwrap();
+            }
+            _ => {}
+        },
+        Err(e) => println!("watch error: {:?}", e),
+    })
+    .unwrap();
     if let Some(dir) = maybe_dir {
-        watcher.watch(Path::new(&dir), RecursiveMode::Recursive).unwrap();
+        watcher
+            .watch(Path::new(&dir), RecursiveMode::Recursive)
+            .unwrap();
     }
 
     // Run the application
     let empty: Vec<String> = vec![];
-    let exit_code = app.run_with_args(&empty);
-    exit_code
+    app.run_with_args(&empty)
 }
 
 fn build_ui(app: &Application, clops: &Args, counter: Arc<Mutex<Situation>>) {
-    let mut update_counter: Rc<RefCell<i32>> = Rc::new(RefCell::new(0));
+    let update_counter: Rc<RefCell<i32>> = Rc::new(RefCell::new(0));
     let mut css = home::home_dir().expect("could not locate home dir");
     css.push(".config");
     css.push("amkhlv");
     css.push("svg-show.css");
     let provider = CssProvider::new();
-    println!("using css: {}",css.to_str().unwrap());
+    println!("using css: {}", css.to_str().unwrap());
     provider.load_from_path(css.to_str().unwrap());
     let screen = gdk4::Display::default().unwrap();
     gtk::style_context_add_provider_for_display(&screen, &provider, 799);
@@ -110,29 +110,25 @@ fn build_ui(app: &Application, clops: &Args, counter: Arc<Mutex<Situation>>) {
     //window.set_child(Some(&drawing_area));
     if let Some(filepath) = clops.file.clone() {
         //let pixbuf = gdk_pixbuf::Pixbuf::from_file(&Path::new(&filepath)).unwrap();
-        let image = gtk::Image::from_file(&Path::new(&filepath));
+        let image = gtk::Image::from_file(Path::new(&filepath));
         window.set_child(Some(&image));
     }
     let counter1 = counter.clone();
     let update_counter1 = update_counter.clone();
-    window.set_default_size(clops.width.unwrap_or(1024),clops.height.unwrap_or(800));
+    window.set_default_size(clops.width.unwrap_or(1024), clops.height.unwrap_or(800));
     let window1 = window.clone();
-    glib::source::timeout_add_local(
-        std::time::Duration::from_millis(250), 
-        move || {
-            let s = counter1.lock().unwrap();
-            //println!("counter is {}, update_counter is {}", s.count, update_counter1.borrow());
-            let i = update_counter1.replace(
-                s.count
-            );
-            if s.count > i {
-                if let Some(p) = s.path.clone() {
-                    let image = gtk::Image::from_file(&p);
-                    window1.set_child(Some(&image));
-                }
+    glib::source::timeout_add_local(std::time::Duration::from_millis(250), move || {
+        let s = counter1.lock().unwrap();
+        //println!("counter is {}, update_counter is {}", s.count, update_counter1.borrow());
+        let i = update_counter1.replace(s.count);
+        if s.count > i {
+            if let Some(p) = s.path.clone() {
+                let image = gtk::Image::from_file(p);
+                window1.set_child(Some(&image));
             }
-            glib::ControlFlow::Continue 
-        });
+        }
+        glib::ControlFlow::Continue
+    });
 
     // Present window
 
