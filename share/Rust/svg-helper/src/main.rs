c179b313aedf289b26f5758293f0867b00d38d42
Andrei Mikhailov <a.mkhlv@gmail.com>
2023-09-27 17:13:32 -0300

    hyperlink

--- share/Rust/svg-helper/src/main.rs
+++ share/Rust/svg-helper/src/main.rs
@@ -47,7 +47,7 @@ fn build_ui(app: &Application) {
     provider.load_from_path(css.to_str().unwrap());
     let screen = gdk4::Display::default().unwrap();
     gtk::style_context_add_provider_for_display(&screen, &provider, 799);
-    let clipboard = screen.clipboard();
+    let clipboard = Rc::new(RefCell::new(screen.clipboard()));
 
     let nx_entry_label = Label::new(Some(" Nx:"));
     let nx_entry = Entry::new();
@@ -59,24 +59,24 @@ fn build_ui(app: &Application) {
     ny_entry.set_max_width_chars(2);
     ny_entry.set_text("5");
 
-    let button = Button::builder()
+    let button_grid = Button::builder()
         .label("grid")
         .margin_top(12)
         .margin_bottom(12)
         .margin_start(12)
         .margin_end(12)
         .build();
-    button.set_css_classes(&["steam-down"]);
+    button_grid.set_css_classes(&["steam-down"]);
 
     let vbox = Box::new(gtk::Orientation::Vertical, 5);
 
-    let hbox = Box::new(gtk::Orientation::Horizontal, 5);
-    hbox.append(&nx_entry_label);
-    hbox.append(&nx_entry);
-    hbox.append(&ny_entry_label);
-    hbox.append(&ny_entry);
-    hbox.append(&button);
-    vbox.append(&hbox);
+    let hbox_grid = Box::new(gtk::Orientation::Horizontal, 5);
+    hbox_grid.append(&nx_entry_label);
+    hbox_grid.append(&nx_entry);
+    hbox_grid.append(&ny_entry_label);
+    hbox_grid.append(&ny_entry);
+    hbox_grid.append(&button_grid);
+    vbox.append(&hbox_grid);
     let window = ApplicationWindow::builder()
         .application(app)
         .title("svg helper")
@@ -85,9 +85,11 @@ fn build_ui(app: &Application) {
 
     let nxentry = Rc::new(RefCell::new(nx_entry));
     let nyentry = Rc::new(RefCell::new(ny_entry));
-    button.connect_clicked(clone!(@weak app => move |button| {
+    let clipboard1 = clipboard.clone();
+    button_grid.connect_clicked(clone!(@weak app => move |button| {
         let nxentry1 = nxentry.clone();
         let nyentry1 = nyentry.clone();
+        let clipboard = clipboard1.clone();
         let nx = nxentry1.borrow().buffer().text().parse::<u32>().unwrap();
         let ny = nyentry1.borrow().buffer().text().parse::<u32>().unwrap();
         let marg = 10;
@@ -119,10 +121,39 @@ fn build_ui(app: &Application) {
             )
             .add(path);
 
-        clipboard.set_text(&document.to_string());
+        clipboard.borrow_mut().set_text(&document.to_string());
         app.quit();
     }));
+    let hbox_href = Box::new(gtk::Orientation::Horizontal, 5);
+    let label_href_txt = Label::new(Some(" Text:"));
+    let en_href_txt = Entry::new();
+    let label_href_url = Label::new(Some(" URL:"));
+    let en_href_url = Entry::new();
+    let btn_href = Button::builder().label("hyperlink").build();
+    hbox_href.append(&label_href_txt);
+    hbox_href.append(&en_href_txt);
+    hbox_href.append(&label_href_url);
+    hbox_href.append(&en_href_url);
 
+    vbox.append(&hbox_href);
+    let rc_en_href_txt = Rc::new(RefCell::new(en_href_txt));
+    let rc_en_href_url = Rc::new(RefCell::new(en_href_url));
+    let clipboard1 = clipboard.clone();
+    btn_href.connect_clicked(clone!(@weak app => move |button| {
+        let clipboard = clipboard1.clone();
+        let rc_en_href_txt = rc_en_href_txt.clone();
+        let rc_en_href_url = rc_en_href_url.clone();
+        let link = svg::node::element::Link::new()
+            .add(svg::node::element::Text::new()
+                 .add(svg::node::element::TSpan::new()
+                      .set("style", "text-decoration:underline;fill:blue")
+                      .add(svg::node::Text::new(rc_en_href_txt.borrow().buffer().text().to_string()))))
+            .set("xlink:href",rc_en_href_url.borrow().buffer().text().to_string());
+        let document = Document::new().add(link);
+        clipboard.borrow_mut().set_text(&document.to_string());
+        app.quit();
+    }));
+    hbox_href.append(&btn_href);
     // Present window
     window.present();
 }
