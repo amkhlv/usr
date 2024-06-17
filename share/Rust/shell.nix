b910266abe82ef03ec67f4a489e713edcf838570
Andrei Mikhailov <a.mkhlv@gmail.com>
2024-06-16 23:18:48 -0300

    use nixpkgs-mozilla for Rust

--- share/Rust/shell.nix
+++ share/Rust/shell.nix
@@ -1,7 +1,38 @@
-with (import <nixpkgs> {});
-mkShell {
-  nativeBuildInputs = [ pkg-config gcc udev ];
-  buildInputs = [
-    cargo rustc rust-analyzer fltk cmake glibc glib pkg-config libadwaita xorg.libxcb xorg.libXmu xorg.xcbutilwm gdk-pixbuf pango gtk3 gtk4 openssl cairo pango ncurses udev 
-  ];  
-}
+
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  rustChannel = pkgs.rustChannelOf {
+     channel = "stable";
+  };
+  rust = (rustChannel.rust.override {
+    targets = [
+      "x86_64-unknown-linux-musl"
+    ];
+    extensions = ["rust-src" "rust-analysis"];
+  });
+in
+  with pkgs;
+  mkShell {
+    nativeBuildInputs = [ pkg-config gcc udev ];
+    buildInputs = [
+      cargo-zigbuild
+      rust
+      cmake 
+      glib 
+      pkg-config 
+      libadwaita 
+      xorg.libxcb 
+      xorg.libXmu 
+      xorg.xcbutilwm 
+      gdk-pixbuf 
+      pango 
+      gtk3 
+      gtk4 
+      openssl 
+      cairo 
+      pango 
+      ncurses 
+      udev 
+    ];  
+  }
